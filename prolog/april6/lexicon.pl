769b5da089bcf8e05fa91ea21df1f16c610e98ee
Jim O'Regan <jaoregan@tcd.ie>
2017-09-04 21:14:50 +0100

    stuff I do not need ever again

--- prolog/april6/lexicon.pl
+++
@@ -1,8 +0,0 @@
-word(ugly,adj,int).
-word(cat,cat,n,s).
-word(dog,dog,n,s).
-word(dogs,dog,n,p).
-word(the,det,quantification(in(X0,X1),in(Y0,Y1),
-	((exists(X0) ^ subset(X1,Y1)) ^ unique(X0)), R,S)) :-
-	newvar0([X0,X1,Y0,Y1],4).
-
--- prolog/simplenp.pl
+++
@@ -1,9 +0,0 @@
-dpl --> [many].
-dsg --> [a].
-dsp --> [the].
-nsg --> [dog].
-npl --> [dogs].
-npsg --> dsg, nsg.
-npsg --> dsp, nsg.
-nppl --> dpl, npl.
-nppl --> dsp, npl.
